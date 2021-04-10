f05a305dbfded7c90585afccf47a2101b7c784fa
Shailesh kumar Satapathy <shaileshsatapathy9@gmail.com>
2021-04-10 12:55:54 +0530

    line comparison added

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace LineComparison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the value of x1: ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of x2: ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of y1: ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of y2: ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Double Lengthoftheline = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2));
+            Console.WriteLine("Length of the line is : " +Lengthoftheline);
+            Console.ReadLine();
         }
     }
 }
