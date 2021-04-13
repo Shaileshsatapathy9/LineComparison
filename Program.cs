2ec1efee5693a5d152dfbbe241eeae82d7c33468
Shailesh kumar Satapathy <shaileshsatapathy9@gmail.com>
2021-04-13 15:50:32 +0530

    oops concept added

--- Program.cs
+++ Program.cs
@@ -1,55 +1,72 @@
 ﻿using System;
 
-namespace LineComparison
+namespace CartesianSyayem
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void compareTwoMethod()
         {
             Console.WriteLine("Enter the value of x1: ");
-            int x1 = Convert.ToInt32(Console.ReadLine());
+            string x1val = Console.ReadLine();
+            int x1 = Convert.ToInt32(x1val);
 
             Console.WriteLine("Enter the value of x2: ");
-            int x2 = Convert.ToInt32(Console.ReadLine());
+            string x2val = Console.ReadLine();
+            int x2 = Convert.ToInt32(x2val);
 
             Console.WriteLine("Enter the value of y1: ");
-            int y1 = Convert.ToInt32(Console.ReadLine());
+            string y1val = Console.ReadLine();
+            int y1 = Convert.ToInt32(y1val);
 
             Console.WriteLine("Enter the value of y2: ");
-            int y2 = Convert.ToInt32(Console.ReadLine());
+            string y2val = Console.ReadLine();
+            int y2 = Convert.ToInt32(y2val);
 
             Console.WriteLine("Enter the value of p1: ");
-            int p1 = Convert.ToInt32(Console.ReadLine());
+            string p1val = Console.ReadLine();
+            int p1 = Convert.ToInt32(p1val);
 
             Console.WriteLine("Enter the value of p2: ");
-            int p2 = Convert.ToInt32(Console.ReadLine());
+            string p2val = Console.ReadLine();
+            int p2 = Convert.ToInt32(p2val);
 
             Console.WriteLine("Enter the value of q1: ");
-            int q1 = Convert.ToInt32(Console.ReadLine());
+            string q1val = Console.ReadLine();
+            int q1 = Convert.ToInt32(q1val);
 
             Console.WriteLine("Enter the value of q2: ");
-            int q2 = Convert.ToInt32(Console.ReadLine());
+            string q2val = Console.ReadLine();
+            int q2 = Convert.ToInt32(q2val);
 
-            Double Lengthoftheline1 = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2));
-            Double Lengthoftheline2 = Math.Sqrt(Math.Pow((p2-p1), 2) + Math.Pow((q2-q1), 2));
+            double lengthOfLine1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("The length of line 1 is: " + lengthOfLine1);
 
-            Console.WriteLine("Length of the line is 1 : " +Lengthoftheline1);
-            Console.WriteLine("Length of the line is 2 : " +Lengthoftheline2);
-            Console.WriteLine(Lengthoftheline1.Equals(Lengthoftheline2));
+            double lengthOfLine2 = Math.Sqrt(Math.Pow((p2 - p1), 2) + Math.Pow((q2 - q1), 2));
+            Console.WriteLine("The length of line 2 is: " + lengthOfLine2);
 
-            if (Lengthoftheline1 == Lengthoftheline2)
+            Console.WriteLine(lengthOfLine1.Equals(lengthOfLine2));
+
+            if (lengthOfLine1 == lengthOfLine2)
             {
-                Console.WriteLine("Both lines are equal");
+                Console.WriteLine("Two lines are equal");
             }
-            else if (Lengthoftheline1 > Lengthoftheline2)
+            else if (lengthOfLine1 > lengthOfLine2)
             {
-                Console.WriteLine("Length of line one is greater than length of line two");
+                Console.WriteLine("lengthOfLine1 is greater than lengthOfLine2");
+            }
+            else if (lengthOfLine1 < lengthOfLine2)
+            {
+                Console.WriteLine("lengthOfLine1 is less than lengthOfLine2");
             }
             else
             {
-                Console.WriteLine("Length of line one is smaller than length of line two");
+                Console.WriteLine("*");
             }
-                Console.ReadLine();
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            compareTwoMethod();
         }
     }
 }
